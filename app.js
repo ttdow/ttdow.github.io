4b4d891687f9275e181ef1770f5016a3f08dc607
ttdow <travis.t.dow@gmail.com>
2022-12-08 17:42:50 -0700

    Reorganizing.

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")// Access the device camera and stream to cameraView
-function cameraStart() {
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+// Access the device camera and stream to cameraView
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraTrigger = document.querySelector("#camera--trigger");
+
+function cameraStart() 
+{
     navigator.mediaDevices
         .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
-}// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
+        .then(function(stream) 
+        {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() 
+{
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-};// Start the video stream when the window loads
+};
+
+// Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);

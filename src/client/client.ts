7edf959c006c41a6f6c070f5d1f62800ad25ae7d
ttdow <travis.t.dow@gmail.com>
2022-12-13 14:41:16 -0700

    Geolocation API test.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,11 @@
 import * as THREE from 'three'
+import { GUI } from 'dat.gui'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 const scene = new THREE.Scene()
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.0)
+scene.add(ambientLight)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -8,20 +13,75 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 2
+camera.position.set(0, 1, 1.0)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setClearColor(0x87CEEB, 1.0)
 document.body.appendChild(renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
-    wireframe: true,
+    wireframe: false,
 })
 
 const cube = new THREE.Mesh(geometry, material)
+cube.scale.set(200, 1, 200)
+cube.position.set(0, -75, -200)
 scene.add(cube)
+
+let mixer: THREE.AnimationMixer
+let modelReady = false
+const animationActions: THREE.AnimationAction[] = []
+let activeAction: THREE.AnimationAction
+let lastAction: THREE.AnimationAction
+
+const fbxLoader = new FBXLoader()
+fbxLoader.load(
+    'models/model.fbx',
+    (object) => {
+        object.position.set(0, 0, -1)
+        object.scale.set(0.01, 0.01, 0.01)
+        mixer = new THREE.AnimationMixer(object)
+
+        const animationAction = mixer.clipAction(
+            (object as THREE.Object3D).animations[0]
+        )
+        animationActions.push(animationAction)
+        animationsFolder.add(animations, 'default')
+        activeAction = animationActions[0]
+
+        scene.add(object)
+
+        // Add an animation from another file
+        fbxLoader.load(
+            'models/samba.fbx',
+            (object) => {
+                console.log('loaded samba')
+                modelReady = true
+                
+                const animationAction = mixer.clipAction(
+                    (object as THREE.Object3D).animations[0]
+                )
+                animationActions.push(animationAction)
+                animationsFolder.add(animations, 'samba')
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            (error) => {
+                console.log(error)
+            }
+        )
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -31,16 +91,50 @@ function onWindowResize() {
     render()
 }
 
-function animate() {
+const animations = {
+    default: function() {
+        setAction(animationActions[0])
+    },
+    samba: function() {
+        setAction(animationActions[1])
+    },
+    test: function() {
+        console.log('WHAT!?')
+    }
+}
+
+const setAction = (toAction: THREE.AnimationAction) => {
+
+    console.log('setAction() was called.')
+
+    if (toAction != activeAction) 
+    {
+        lastAction = activeAction
+        activeAction = toAction
+        lastAction.fadeOut(1)
+        activeAction.reset()
+        activeAction.fadeIn(1)
+        activeAction.play()
+    }
+}
+
+const gui = new GUI()
+const animationsFolder = gui.addFolder('Animations')
+animationsFolder.open()
+
+const clock = new THREE.Clock()
+
+function animate()
+{
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    if (modelReady) mixer.update(clock.getDelta())
 
     render()
 }
 
-function render() {
+function render() 
+{
     renderer.render(scene, camera)
 }
 

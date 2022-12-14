811d3f9e59d7ac531d8455418b0178079152b741
ttdow <travis.t.dow@gmail.com>
2022-12-13 19:06:39 -0700

    Weird things happening.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,11 +1,6 @@
 import * as THREE from 'three'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
-/*
-window.onload = () => {
-    var button = document.querySelector('button[data-action]="change"') as HTMLElement
-}*/
-
 document.querySelector('button[data-action="change"')?.addEventListener('click', function() {
     setAction(animationActions[1])
 })
